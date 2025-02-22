79eeb376676ab9b72a3a39477a5321c632371541
hkamthe4980 <hkamthe8@gmail.com>
2025-02-22 10:29:08 +0530

    Added new files, modified index.js & input.js, deleted some files

--- index.js
+++ index.js
@@ -24,7 +24,73 @@
 
 
 
-const colors = require('colors');
-console.log("package".green);
+// const colors = require('colors');
+// console.log("package".green);
 
 
+// console.warn("Hemant kamthe");
+
+
+// const http = require('http');
+// const data = require('./input')
+// http.createServer((req,resp)=>{
+//     resp.writeHead(201,{'Content-Type':'application\json'});
+//     resp.write(JSON.stringify(data))
+//     resp.end();
+// }).listen(5000);
+
+
+// const fs = require('fs');
+// const input = process.argv;
+
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3],input[4])
+// }
+// else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3])
+// }
+// else{
+//     console.log("Invalid Input");
+// }
+
+// console.log(process.argv);
+
+//Display files list from folder
+// const fs = require('fs');
+// const path = require('path');
+
+// const dirpath = path.join(__dirname,'files');
+// console.log(dirpath);
+
+// for(let i=0;i<5;i++){
+//     fs.writeFileSync(`${dirpath}/hello${i}.txt`,"Sample Files is Here");
+
+// }
+
+// fs.readdir(dirpath,(err,files)=>{
+// files.forEach((item)=>{
+//     console.log("File Name is:" ,item);
+
+// })
+
+// })
+
+//create a file in specific folder
+
+
+const fileSystem = require('fs');
+const path = require('path');
+const FilePath = path.join(__dirname,'CRUD');
+const dirFileName = (`${FilePath}/crud.txt`)
+// fileSystem.writeFileSync(dirFileName,'This is the crud file');
+
+fileSystem.readFile(dirFileName,'utf-8',(err,item)=>{
+    console.log(item);
+
+})
+fileSystem.appendFile(dirFileName,'We need to append this msg into a file',(err)=>{
+    if(!err){
+        console.log('file is up to date ');
+    }
+
+});
